ba16edce79488d2a95c5acce97ef269f959e645b
Jason-Young-AI <AI.Jason.Young@outlook.com>
2022-06-03 13:04:58 +0800

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LostComponent } from './lost/lost.component';
+import { MainComponent } from './main/main.component';
+import { AdminComponent } from './admin/admin.component';
+import { HiddenComponent } from './hidden/hidden.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'Main' },
+
+  { path: 'Main', component: MainComponent, loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: 'Admin', component: AdminComponent, loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'Hidden', component: HiddenComponent, loadChildren: () => import('./hidden/hidden.module').then(m => m.HiddenModule) },
+
+  { path: '**', component: LostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
