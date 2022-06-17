aa9b85629c837f1c62e057d2a6e22daf2a7b6f6e
Jason-Young-AI <AI.Jason.Young@outlook.com>
2022-06-17 10:30:56 +0800

    Add new component SignIn & SignUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LostComponent } from './lost/lost.component';
 import { MainComponent } from './main/main.component';
 import { AdminComponent } from './admin/admin.component';
 import { HiddenComponent } from './hidden/hidden.component';
+
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+import { LostComponent } from './lost/lost.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'Main' },
@@ -12,6 +16,9 @@ const routes: Routes = [
   { path: 'Main', component: MainComponent, loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
   { path: 'Admin', component: AdminComponent, loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'Hidden', component: HiddenComponent, loadChildren: () => import('./hidden/hidden.module').then(m => m.HiddenModule) },
+
+  { path: 'SignIn', component: SigninComponent },
+  { path: 'SignUp', component: SignupComponent },
 
   { path: '**', component: LostComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LostComponent } from './lost/lost.component';
+
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LostComponent
+    LostComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
