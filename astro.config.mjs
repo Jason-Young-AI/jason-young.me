85e95148fdb1e9571b455ef6938770893dfc0247
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-09-06 23:11:28 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,27 @@ import sitemap from "@astrojs/sitemap"
 import solidJs from "@astrojs/solid-js"
 import tailwind from "@astrojs/tailwind"
 
+import remarkToc from 'remark-toc';
+import remarkMath from 'remark-math'
+
+import rehypeKatex from 'rehype-katex'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [
+      remarkMath,
+      [remarkToc, { heading: "contents"} ],
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'append' }]
+    ],
+  },
   site: "https://jason-young.me",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
